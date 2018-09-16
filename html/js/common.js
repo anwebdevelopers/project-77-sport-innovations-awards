582de66cf0433c0f7fc7d71febbb979a376dec08
anwebdevelopers <anwebdev78@gmail.com>
2018-09-16 18:55:48 +0400

    16.09.2018

---
+++ html/js/common.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    'use strict';
+
+    $('.about').addClass('owl-carousel').owlCarousel({
+        loop: false,
+        nav: true,
+        navText: '',
+        items: 1,
+        smartSpeed: 1000,
+        // autoHeight: true,
+        autoplay: false
+    });
+
+});
